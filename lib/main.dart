e49fc281a3ae055be60edc3303e2d0570aa0a4fc
duydlm <duydlm@vng.com.vn>
2025-04-02 22:11:20 +0700

    Add log Bloc to debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_todos_bloc_hive/services/local_storage_todos_api.dart';
 
 import 'app/app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = const AppBlocObserver();
   var localStorageTodosApi = LocalStorageTodosApi();
   await localStorageTodosApi.init();
   runApp(App(
     todosApi: localStorageTodosApi,
   ));
+}
+
+/// {@template app_bloc_observer}
+/// Custom [BlocObserver] that observes all bloc and cubit state changes.
+/// {@endtemplate}
+class AppBlocObserver extends BlocObserver {
+  /// {@macro app_bloc_observer}
+  const AppBlocObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    if (bloc is Cubit) print(change);
+  }
+
+  @override
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
 }
